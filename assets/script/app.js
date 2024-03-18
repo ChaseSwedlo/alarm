c37be839d586aa4548c7d6d5cbcf6f358f9190e4
Chase Swedlo <chasechaseswedlo@outlook.com>
2024-03-18 13:41:14 -0500

    Add current time and input validation

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const mainTime = document.querySelector('.time');
+const setAlarmTime = document.querySelector('.set-alarm-time');
+const hoursInput = document.querySelector('.hours');
+const minutesInput = document.querySelector('.minutes');
+
+function updateCurrentTime() {
+    const currentTime = new Date();
+    const hours = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+    mainTime.textContent = `${hours}:${minutes}`;
+}
+updateCurrentTime();
+setInterval(updateCurrentTime, 1000);
+
+function validateInput(input) {
+    let value = input.value;
+    input.value = value.replace(/\D/g, '').slice(0, 2);
+}
+hoursInput.addEventListener('input', () => {
+    validateInput(hoursInput);
+});
+minutesInput.addEventListener('input', () => {
+    validateInput(minutesInput);
+});
